7f58009bb544a3dfae3bac97fa3922a366a2d9ef
mtxz <mtxz@git.com>
2021-07-15 11:29:57 +0800

    2021/7/15 commit!

---
+++ 06spring4_autowiring/src/cn/sxt/service/UserService.java
@@ -0,0 +1,11 @@
+package cn.sxt.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * author: ylb
+ * Date: 2021/7/12
+ * Time: 9:39
+ */
+public interface UserService {
+    public void getUser();
+}
---
+++ 06spring4_autowiring/src/cn/sxt/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package cn.sxt.service.impl;
+
+import cn.sxt.dao.UserDao;
+import cn.sxt.service.UserService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * author: ylb
+ * Date: 2021/7/12
+ * Time: 9:41
+ */
+public class UserServiceImpl implements UserService {
+    private UserDao userDao = null;
+
+    public UserServiceImpl() {
+    }
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public void getUser() {
+        userDao.getUser();
+    }
+}
